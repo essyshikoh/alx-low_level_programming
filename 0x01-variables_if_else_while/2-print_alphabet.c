a0699f63ba3fd35f295e482860727abbe4731c47
essyshikoh <essyshikoh2013@gmail.com>
2022-07-02 05:47:46 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char 1 = 97;
+
+	while (1 <= 122)
+	{
+		putchar(1);
+		++1;
+	}
+	putchar(10);
+
+	return (0);
+}
