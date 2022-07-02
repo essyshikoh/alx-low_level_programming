b8d4c86d1780cae91facc72a81aba2585edceb28
essyshikoh <essyshikoh2013@gmail.com>
2022-07-02 08:30:00 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char low = 97;
+	char up = 65;
+
+	while (low <= 122)
+	{
+		putchar(low);
+		++low;
+	}
+	while (up <= 90)
+	{
+		putchar(up);
+		++up;
+	}
+
+	putchar(10);
+
+	return (0);
+}
