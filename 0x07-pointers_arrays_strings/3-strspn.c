b5cf565983a6c37093af98cc251e35093f9adc3c
essyshikoh <essyshikoh2013@gmail.com>
2022-07-18 13:10:44 -0700

    correction task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,16 +10,20 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int x, y = 0, z = 0, k = 0;
 
+
 	for (x = 0; *(s + x) != 0; x++)
 	{
 		k = 0;
 		for (y = 0;  *(accept + y) != 0; y++)
 		{
 			if (*(s + x) == *(accept + y))
+			{
 				z++;
-			k = 1;
+				k = 1;
+			}
 		}
+		if (k == 0)
+			return (z);
 	}
-	if (k == 0)
-		return (z);
+	return (0);
 }
