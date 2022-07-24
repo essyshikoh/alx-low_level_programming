7f85034cda0fd61e560b1fc531811c96234cdaef
essyshikoh <essyshikoh2013@gmail.com>
2022-07-24 14:12:53 -0700

    this was only created for the purpose of static library task

---
+++ 0x05-pointers_arrays_strings/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * abs - function to get the absolute value
+ *  @n: parameter of the funtion
+ *  Return: the absolute value
+ */
+int _isupper(int c);
+{
+	return (0);
+}
--- 0x05-pointers_arrays_strings/0-strncat.c
+++ 0x05-pointers_arrays_strings/0-strncat.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include "main'h"
 
 /**
  * _abs - function to get the absolute value
  * @n: parameter of the funtion
  * Return: the absolute value
  */
-int _atoi(char *s);
+char *_strncat(char *dest, char *src, int n);
 {
 	return (0);
 }
